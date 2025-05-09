aca3be18ba53e56966972e1453ccf3a2bd11a13a
OstiumAxioma <ostiumaxioma@outlook.com>
2025-05-09 11:57:23 -0700

    Add course assignment componenta

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -2,11 +2,12 @@ import CourseNavigation from "./Navigation";
 import { Navigate, Route, Routes } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/editor";
 export default function Courses() {
   return (
     <div id="wd-courses">
       <h2>Course 1234</h2>
-      <hr />
       <table>
         <tr>
           <td valign="top">
@@ -17,8 +18,8 @@ export default function Courses() {
               <Route path="/" element={<Navigate to="Home" />} />
               <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<h2>Assignments</h2>} />
-              <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
+              <Route path="Assignments" element={<Assignments />} />
+              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
               <Route path="People" element={<h2>People</h2>} />
             </Routes>
           </td>
