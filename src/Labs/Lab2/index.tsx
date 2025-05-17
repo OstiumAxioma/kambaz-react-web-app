4f5af121f2300c732d5b55611c4f02c08b6a4b79
OstiumAxioma <ostiumaxioma@outlook.com>
2025-05-17 09:48:50 +0800

    Selecting HTML Content Based on the Document Structure

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -20,5 +20,22 @@ export default function Lab2() {
           This heading has same style as paragraph above.
         </h4>
       </div>
+      <div id="wd-css-document-selectors">
+        <div className="wd-selector-1">
+          <h3>Document Structure Selectors</h3>
+          <div className="wd-selector-2">
+            Selectors can be combined to refer elements in partivular places in the document.
+            <p className="wd-selector-3">
+            This paragraph's red background is referenced as
+            <br />.selector-2 .selector3<br />
+            meaning the descendant of some ancestor.<br />
+            <span className="wd-selector-4">
+              Whereas this span is a direct child of its parent
+            </span><br />
+            You can combine these relationships to create specific styles depending on the document structure
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );}
