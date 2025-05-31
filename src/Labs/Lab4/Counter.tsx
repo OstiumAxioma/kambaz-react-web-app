53d0c99139abe166d65a367301ab4d3718c576be
OstiumAxioma <ostiumaxioma@outlook.com>
2025-05-31 10:36:56 +0800

    a4 start

---
+++ src/Labs/Lab4/Counter.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(7);
+  const padding10px = { padding: "10px" };
+  const buttonBaseStyle = {
+    ...padding10px,
+    color: "black",
+    border: "none",
+    cursor: "pointer",
+    margin: "0 5px"
+  };
+  const upVoteStyle = {
+    ...buttonBaseStyle,
+    backgroundColor: "lightgreen"
+  };
+  const downVoteStyle = {
+    ...buttonBaseStyle,
+    backgroundColor: "lightcoral"
+  };
+
+  console.log(count);
+  return (
+    <div id="wd-counter-use-state">
+      <h2>Counter: {count}</h2>
+      <button
+        style={upVoteStyle}
+        onClick={() => { setCount(count + 1); console.log(count); }}
+        id="wd-counter-up-click">Up</button>
+      <button
+        style={downVoteStyle}
+        onClick={() => { setCount(count - 1); console.log(count); }}
+        id="wd-counter-down-click">Down</button>
+      <hr/>
+    </div>
+  );
+}
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -22,6 +22,11 @@ export default function TOC() {
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
+        <Nav.Link as={Link} to="/Labs/Lab4" id="wd-a4"active={pathname.includes("Lab4")}>
+          Lab 4
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
         <Nav.Link to="/Kambaz" as={Link} active={pathname === "/Kambaz"}>
           Kambaz
         </Nav.Link>
