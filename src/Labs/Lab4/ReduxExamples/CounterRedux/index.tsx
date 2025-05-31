6583c0ce67c595a44795d1485831a401d17b619a
Zhong Yun <zheng.zhi5@northeastern.edu>
2025-05-31 17:05:17 +0800

    a4 update

---
+++ src/Labs/Lab4/ReduxExamples/CounterRedux/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterReducer";
+
+export default function CounterRedux() {
+  const count = useSelector((state: any) => state.counterReducer.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h3>Counter Redux Example</h3>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+} 
