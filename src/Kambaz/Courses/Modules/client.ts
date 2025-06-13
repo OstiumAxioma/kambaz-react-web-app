f59f64f37b67ecf279ea885cf0c635c75dcf4b6d
Zhong Yun <zheng.zhi5@northeastern.edu>
2025-06-14 00:51:00 +0800

    route path change

--- src/Kambaz/Courses/Modules/client.ts
+++ src/Kambaz/Courses/Modules/client.ts
@@ -7,7 +7,7 @@ const MODULES_API = `${REMOTE_SERVER}/api/modules`;
 
 export const findModulesForCourse = async (courseId: string) => {
   console.log("Fetching modules for course:", courseId);
-  const response = await axiosWithCredentials.get(`${MODULES_API}/${courseId}/modules`);
+  const response = await axiosWithCredentials.get(`${MODULES_API}/${courseId}`);
   console.log("Modules response:", response.data);
   return response.data;
 };
